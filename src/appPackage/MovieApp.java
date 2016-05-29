655905f8f48860e4212d0a696395cb21b9afd64d
Calvin Brooks <brooks.calvin4@gmail.com>
2016-05-28 23:15:25 -0400

    added MovieIO to for loop.

--- src/appPackage/MovieApp.java
+++ src/appPackage/MovieApp.java
@@ -11,13 +11,13 @@ public class MovieApp {
 		//Movie m = new Movie();
 		ArrayList<Movie>myList = new ArrayList<>();
 		int drama=0;
-		for (int i = 0; i < 3; i++){
-			//myList.add(new Movie());
-			System.out.print("Title :");
-			myList.get(i).setTitle(input.nextLine());
-			System.out.print("Category :");
-			myList.get(i).setCategory(input.nextLine());
-				}
+		for (int i = 0; i < 100; i++){
+			myList.add(MovieIO.getMovie(i+1));
+//			System.out.print("Title :");
+//			myList.get(i).setTitle(input.nextLine());
+//			System.out.print("Category :");
+//			myList.get(i).setCategory(input.nextLine());
+//				}
 		System.out.println("there are "+myList.size()+" movies in this list");
 		System.out.print("What category are you interested in?: ");
 		input.nextLine();
